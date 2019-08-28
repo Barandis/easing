c756f0e1ef759f921abf89e3627063a34f1524aa
Barandis <barandis1729@gmail.com>
2019-08-28 11:30:04 -0400

    Changed easing function names

--- src/easing.js
+++ src/easing.js
@@ -26,80 +26,80 @@ export const linear = t => t;
 
 // Sinusoidal functions
 
-export const inSine = t => 1 - Math.cos(t * PI_OVER_TWO);
+export const sineIn = t => 1 - Math.cos(t * PI_OVER_TWO);
 
-export const outSine = t => Math.sin(t * PI_OVER_TWO);
+export const sineOut = t => Math.sin(t * PI_OVER_TWO);
 
-export const inOutSine = t => -(Math.cos(Math.PI * t) - 1) / 2;
+export const sineInOut = t => -(Math.cos(Math.PI * t) - 1) / 2;
 
 // Quadratic functions
 
-export const inQuad = t => t * t;
+export const quadraticIn = t => t * t;
 
-export const outQuad = t => t * (2 - t);
+export const quadraticOut = t => t * (2 - t);
 
-export const inOutQuad = t => (t < 0.5 ? 2 * t * t : t * (4 - 2 * t) - 1);
+export const quadraticInOut = t => (t < 0.5 ? 2 * t * t : t * (4 - 2 * t) - 1);
 
 // Cubic functions
 
-export const inCubic = t => t * t * t;
+export const cubicIn = t => t * t * t;
 
-export const outCubic = t => {
+export const cubicOut = t => {
   const u = t - 1;
   return 1 + u * u * u;
 };
 
-export const inOutCubic = t => {
+export const cubicInOut = t => {
   const u = t - 1;
   return t < 0.5 ? 4 * t * t * t : 1 + 4 * u * u * u;
 };
 
 // Quartic functions
 
-export const inQuart = t => t * t * t * t;
+export const quarticIn = t => t * t * t * t;
 
-export const outQuart = t => {
+export const quarticOut = t => {
   const u = t - 1;
   return 1 - u * u * u * u;
 };
 
-export const inOutQuart = t => {
+export const quarticInOut = t => {
   const u = t - 1;
   return t < 0.5 ? 8 * t * t * t * t : 1 - 8 * u * u * u * u;
 };
 
 // Quintic functions
 
-export const inQuint = t => t * t * t * t * t;
+export const quinticIn = t => t * t * t * t * t;
 
-export const outQuint = t => {
+export const quinticOut = t => {
   const u = t - 1;
   return 1 + u * u * u * u * u;
 };
 
-export const inOutQuint = t => {
+export const quinticInOut = t => {
   const u = t - 1;
   return t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * u * u * u * u * u;
 };
 
 // Exponential functions
 
-export const inExpo = t => 2 ** (10 * (t - 1));
+export const exponentialIn = t => 2 ** (10 * (t - 1));
 
-export const outExpo = t => 1 - 2 ** (-10 * t);
+export const exponentialOut = t => 1 - 2 ** (-10 * t);
 
-export const inOutExpo = t => {
+export const exponentialInOut = t => {
   const u = t * 2 - 1;
   return t < 0.5 ? 2 ** (10 * u) / 2 : (2 - 2 ** (-10 * u)) / 2;
 };
 
 // Circular functions
 
-export const inCirc = t => 1 - Math.sqrt(1 - t * t);
+export const circularIn = t => 1 - Math.sqrt(1 - t * t);
 
-export const outCirc = t => Math.sqrt(t * (2 - t));
+export const circularOut = t => Math.sqrt(t * (2 - t));
 
-export const inOutCirc = t => {
+export const circularInOut = t => {
   const u = 2 * t;
   const v = u - 2;
   return t < 0.5 ? (Math.sqrt(1 - u * u) - 1) / 2 : (Math.sqrt(1 - v * v) + 1) / 2;
@@ -107,14 +107,14 @@ export const inOutCirc = t => {
 
 // Backwards functions
 
-export const inBack = t => t * t * ((BACK_OVERSHOOT + 1) * t - BACK_OVERSHOOT);
+export const backwardsIn = t => t * t * ((BACK_OVERSHOOT + 1) * t - BACK_OVERSHOOT);
 
-export const outBack = t => {
+export const backwardsOut = t => {
   const u = t - 1;
   return u * u * ((BACK_OVERSHOOT + 1) * u + BACK_OVERSHOOT) + 1;
 };
 
-export const inOutBack = t => {
+export const backwardsInOut = t => {
   const u = 2 * t;
   const v = u - 2;
   const s = BACK_OVERSHOOT * 1.525;
@@ -123,20 +123,20 @@ export const inOutBack = t => {
 
 // Elastic functions
 
-export const inElastic = t => {
+export const elasticIn = t => {
   const p = 0.3;
   const s = (p / TWO_PI) * Math.asin(1);
   const u = t - 1;
   return -(2 ** (10 * u) * Math.sin(((u - s) * TWO_PI) / p));
 };
 
-export const outElastic = t => {
+export const elasticOut = t => {
   const p = 0.3;
   const s = (p / TWO_PI) * Math.asin(1);
   return 2 ** (-10 * t) * Math.sin(((t - s) * TWO_PI) / p) + 1;
 };
 
-export const inOutElastic = t => {
+export const elasticInOut = t => {
   const p = 0.45;
   const s = (p / TWO_PI) * Math.asin(1);
   const u = 2 * t - 1;
@@ -147,7 +147,7 @@ export const inOutElastic = t => {
 
 // Bounce functions
 
-export const outBounce = t => {
+export const bounceOut = t => {
   if (t < BOUNCE_THRESHOLD_1) {
     return BOUNCE_BASE * t * t;
   }
@@ -163,9 +163,9 @@ export const outBounce = t => {
   return BOUNCE_BASE * u * u + BOUNCE_OFFSET_4;
 };
 
-export const inBounce = t => 1 - outBounce(1 - t);
+export const bounceIn = t => 1 - bounceOut(1 - t);
 
-export const inOutBounce = t => (t < 0.5 ? inBounce(2 * t) / 2 : outBounce(2 * t - 1) / 2 + 0.5);
+export const bounceInOut = t => (t < 0.5 ? bounceIn(2 * t) / 2 : bounceOut(2 * t - 1) / 2 + 0.5);
 
 // Compatibility
 
