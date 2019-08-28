b201e7b15f7794319481e37d5dd30486c70466d9
Barandis <barandis1729@gmail.com>
2019-08-28 12:23:19 -0400

    Added utilities to index export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 // Copyright (c) 2019 Thomas J. Otterson
-// 
+//
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
 export * from './easing';
 export * from './bezier';
+export * from './utilities';
