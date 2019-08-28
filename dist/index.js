78ed1f98a3bd267a829878929db86e046b531aea
Barandis <barandis1729@gmail.com>
2019-08-28 12:43:47 -0400

    Build

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,354 @@
-!function(n){var t={};function r(u){if(t[u])return t[u].exports;var e=t[u]={i:u,l:!1,exports:{}};return n[u].call(e.exports,e,e.exports,r),e.l=!0,e.exports}r.m=n,r.c=t,r.d=function(n,t,u){r.o(n,t)||Object.defineProperty(n,t,{enumerable:!0,get:u})},r.r=function(n){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(n,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(n,"__esModule",{value:!0})},r.t=function(n,t){if(1&t&&(n=r(n)),8&t)return n;if(4&t&&"object"==typeof n&&n&&n.__esModule)return n;var u=Object.create(null);if(r.r(u),Object.defineProperty(u,"default",{enumerable:!0,value:n}),2&t&&"string"!=typeof n)for(var e in n)r.d(u,e,function(t){return n[t]}.bind(null,e));return u},r.n=function(n){var t=n&&n.__esModule?function(){return n.default}:function(){return n};return r.d(t,"a",t),t},r.o=function(n,t){return Object.prototype.hasOwnProperty.call(n,t)},r.p="",r(r.s=0)}([function(n,t,r){"use strict";r.r(t);var u=2*Math.PI,e=Math.PI/2,i=function(n){return n},o=function(n){return 1-Math.cos(n*e)},c=function(n){return Math.sin(n*e)},f=function(n){return-(Math.cos(Math.PI*n)-1)/2},a=function(n){return n*n},d=function(n){return n*(2-n)},s=function(n){return n<.5?2*n*n:n*(4-2*n)-1},v=function(n){return n*n*n},l=function(n){var t=n-1;return 1+t*t*t},M=function(n){var t=n-1;return n<.5?4*n*n*n:1+4*t*t*t},h=function(n){return n*n*n*n},p=function(n){var t=n-1;return 1-t*t*t*t},b=function(n){var t=n-1;return n<.5?8*n*n*n*n:1-8*t*t*t*t},O=function(n){return n*n*n*n*n},y=function(n){var t=n-1;return 1+t*t*t*t*t},w=function(n){var t=n-1;return n<.5?16*n*n*n*n*n:1+16*t*t*t*t*t},Q=function(n){return Math.pow(2,10*(n-1))},j=function(n){return 1-Math.pow(2,-10*n)},x=function(n){var t=2*n-1;return n<.5?Math.pow(2,10*t)/2:(2-Math.pow(2,-10*t))/2},P=function(n){return 1-Math.sqrt(1-n*n)},S=function(n){return Math.sqrt(n*(2-n))},m=function(n){var t=2*n,r=t-2;return n<.5?(Math.sqrt(1-t*t)-1)/2:(Math.sqrt(1-r*r)+1)/2},B=function(n){return n*n*(2.70158*n-1.70158)},g=function(n){var t=n-1;return t*t*(2.70158*t+1.70158)+1},C=function(n){var t=2*n,r=t-2,u=2.5949095;return n<.5?t*t*((u+1)*t-u)/2:(r*r*((u+1)*r+u)+2)/2},E=function(n){var t=.3/u*Math.asin(1),r=n-1;return-Math.pow(2,10*r)*Math.sin((r-t)*u/.3)},_=function(n){var t=.3/u*Math.asin(1);return Math.pow(2,-10*n)*Math.sin((n-t)*u/.3)+1},k=function(n){var t=.45/u*Math.asin(1),r=2*n-1;return n<.5?-Math.pow(2,10*r)*Math.sin((r-t)*u/.45)/2:Math.pow(2,-10*r)*Math.sin((r-t)*u/.45)/2+1},q=function(n){if(n<1/2.75)return 7.5635*n*n;if(n<2/2.75){var t=n-1.5/2.75;return 7.5635*t*t+.75}if(n<2.5/2.75){var r=n-2.25/2.75;return 7.5635*r*r+.9375}var u=n-2.625/2.75;return 7.5635*u*u+.984375},I=function(n){return 1-q(1-n)},T=function(n){return n<.5?I(2*n)/2:q(2*n-1)/2+.5},z=function(n){return function(t,r,u,e,i){return u+n(r/i)*(e-u)}},A=4,F=.001,D=1e-7,G=10,H=11,J=1/(H-1),K=function(n,t){return 1-3*t+3*n},L=function(n,t){return 3*t-6*n},N=function(n){return 3*n};function R(n,t,r){return(K(t,r)*n+L(t,r))*n+N(t)}function U(n,t,r){return 3*K(t,r)*n*n+2*L(t,r)*n+N(t)}function V(n,t,r,u){if(n===t&&r===u)return i;for(var e=new Float32Array(H),o=0;o<H;o++)e[o]=R(o*J,n,r);function c(t){for(var u=0,i=1,o=H-1;i!==o&&e[i]<t;i++)u+=J;var c=u+(t-e[i-=1])/(e[i+1]-e[i])*J,f=U(c,n,r);return f>=F?function(n,t,r,u){for(var e=t,i=0;i<A;i++){var o=U(e,r,u);if(0===o)break;e-=(R(e,r,u)-n)/o}return e}(t,c,n,r):0===f?c:function(n,t,r,u,e){var i=0,o=t,c=r,f=0;do{R(i=o+(c-o)/2,u,e)-n>0?c=i:o=i}while(Math.abs(n)>D&&++f<G);return i}(t,u,u+J,n,r)}return function(n){return R(c(n),t,u)}}r.d(t,"linear",function(){return i}),r.d(t,"inSine",function(){return o}),r.d(t,"outSine",function(){return c}),r.d(t,"inOutSine",function(){return f}),r.d(t,"inQuad",function(){return a}),r.d(t,"outQuad",function(){return d}),r.d(t,"inOutQuad",function(){return s}),r.d(t,"inCubic",function(){return v}),r.d(t,"outCubic",function(){return l}),r.d(t,"inOutCubic",function(){return M}),r.d(t,"inQuart",function(){return h}),r.d(t,"outQuart",function(){return p}),r.d(t,"inOutQuart",function(){return b}),r.d(t,"inQuint",function(){return O}),r.d(t,"outQuint",function(){return y}),r.d(t,"inOutQuint",function(){return w}),r.d(t,"inExpo",function(){return Q}),r.d(t,"outExpo",function(){return j}),r.d(t,"inOutExpo",function(){return x}),r.d(t,"inCirc",function(){return P}),r.d(t,"outCirc",function(){return S}),r.d(t,"inOutCirc",function(){return m}),r.d(t,"inBack",function(){return B}),r.d(t,"outBack",function(){return g}),r.d(t,"inOutBack",function(){return C}),r.d(t,"inElastic",function(){return E}),r.d(t,"outElastic",function(){return _}),r.d(t,"inOutElastic",function(){return k}),r.d(t,"outBounce",function(){return q}),r.d(t,"inBounce",function(){return I}),r.d(t,"inOutBounce",function(){return T}),r.d(t,"jquerify",function(){return z}),r.d(t,"makeBezier",function(){return V})}]);
+!(function(n) {
+  var t = {};
+  function r(u) {
+    if (t[u]) return t[u].exports;
+    var e = (t[u] = { i: u, l: !1, exports: {} });
+    return n[u].call(e.exports, e, e.exports, r), (e.l = !0), e.exports;
+  }
+  (r.m = n),
+    (r.c = t),
+    (r.d = function(n, t, u) {
+      r.o(n, t) || Object.defineProperty(n, t, { enumerable: !0, get: u });
+    }),
+    (r.r = function(n) {
+      'undefined' != typeof Symbol &&
+        Symbol.toStringTag &&
+        Object.defineProperty(n, Symbol.toStringTag, { value: 'Module' }),
+        Object.defineProperty(n, '__esModule', { value: !0 });
+    }),
+    (r.t = function(n, t) {
+      if ((1 & t && (n = r(n)), 8 & t)) return n;
+      if (4 & t && 'object' == typeof n && n && n.__esModule) return n;
+      var u = Object.create(null);
+      if ((r.r(u), Object.defineProperty(u, 'default', { enumerable: !0, value: n }), 2 & t && 'string' != typeof n))
+        for (var e in n)
+          r.d(
+            u,
+            e,
+            function(t) {
+              return n[t];
+            }.bind(null, e)
+          );
+      return u;
+    }),
+    (r.n = function(n) {
+      var t =
+        n && n.__esModule
+          ? function() {
+              return n.default;
+            }
+          : function() {
+              return n;
+            };
+      return r.d(t, 'a', t), t;
+    }),
+    (r.o = function(n, t) {
+      return Object.prototype.hasOwnProperty.call(n, t);
+    }),
+    (r.p = ''),
+    r((r.s = 0));
+})([
+  function(n, t, r) {
+    'use strict';
+    r.r(t);
+    var u = 2 * Math.PI,
+      e = Math.PI / 2,
+      i = function(n) {
+        return n;
+      },
+      o = function(n) {
+        return 1 - Math.cos(n * e);
+      },
+      c = function(n) {
+        return Math.sin(n * e);
+      },
+      f = function(n) {
+        return -(Math.cos(Math.PI * n) - 1) / 2;
+      },
+      a = function(n) {
+        return n * n;
+      },
+      d = function(n) {
+        return n * (2 - n);
+      },
+      s = function(n) {
+        return n < 0.5 ? 2 * n * n : n * (4 - 2 * n) - 1;
+      },
+      l = function(n) {
+        return n * n * n;
+      },
+      v = function(n) {
+        var t = n - 1;
+        return 1 + t * t * t;
+      },
+      M = function(n) {
+        var t = n - 1;
+        return n < 0.5 ? 4 * n * n * n : 1 + 4 * t * t * t;
+      },
+      h = function(n) {
+        return n * n * n * n;
+      },
+      p = function(n) {
+        var t = n - 1;
+        return 1 - t * t * t * t;
+      },
+      O = function(n) {
+        var t = n - 1;
+        return n < 0.5 ? 8 * n * n * n * n : 1 - 8 * t * t * t * t;
+      },
+      b = function(n) {
+        return n * n * n * n * n;
+      },
+      I = function(n) {
+        var t = n - 1;
+        return 1 + t * t * t * t * t;
+      },
+      w = function(n) {
+        var t = n - 1;
+        return n < 0.5 ? 16 * n * n * n * n * n : 1 + 16 * t * t * t * t * t;
+      },
+      q = function(n) {
+        return Math.pow(2, 10 * (n - 1));
+      },
+      y = function(n) {
+        return 1 - Math.pow(2, -10 * n);
+      },
+      m = function(n) {
+        var t = 2 * n - 1;
+        return n < 0.5 ? Math.pow(2, 10 * t) / 2 : (2 - Math.pow(2, -10 * t)) / 2;
+      },
+      x = function(n) {
+        return 1 - Math.sqrt(1 - n * n);
+      },
+      P = function(n) {
+        return Math.sqrt(n * (2 - n));
+      },
+      g = function(n) {
+        var t = 2 * n,
+          r = t - 2;
+        return n < 0.5 ? (Math.sqrt(1 - t * t) - 1) / 2 : (Math.sqrt(1 - r * r) + 1) / 2;
+      },
+      j = function(n) {
+        return n * n * (2.70158 * n - 1.70158);
+      },
+      S = function(n) {
+        var t = n - 1;
+        return t * t * (2.70158 * t + 1.70158) + 1;
+      },
+      _ = function(n) {
+        var t = 2 * n,
+          r = t - 2,
+          u = 2.5949095;
+        return n < 0.5 ? (t * t * ((u + 1) * t - u)) / 2 : (r * r * ((u + 1) * r + u) + 2) / 2;
+      },
+      k = function(n) {
+        var t = (0.3 / u) * Math.asin(1),
+          r = n - 1;
+        return -Math.pow(2, 10 * r) * Math.sin(((r - t) * u) / 0.3);
+      },
+      T = function(n) {
+        var t = (0.3 / u) * Math.asin(1);
+        return Math.pow(2, -10 * n) * Math.sin(((n - t) * u) / 0.3) + 1;
+      },
+      z = function(n) {
+        var t = (0.45 / u) * Math.asin(1),
+          r = 2 * n - 1;
+        return n < 0.5
+          ? (-Math.pow(2, 10 * r) * Math.sin(((r - t) * u) / 0.45)) / 2
+          : (Math.pow(2, -10 * r) * Math.sin(((r - t) * u) / 0.45)) / 2 + 1;
+      },
+      A = function(n) {
+        if (n < 1 / 2.75) return 7.5635 * n * n;
+        if (n < 2 / 2.75) {
+          var t = n - 1.5 / 2.75;
+          return 7.5635 * t * t + 0.75;
+        }
+        if (n < 2.5 / 2.75) {
+          var r = n - 2.25 / 2.75;
+          return 7.5635 * r * r + 0.9375;
+        }
+        var u = n - 2.625 / 2.75;
+        return 7.5635 * u * u + 0.984375;
+      },
+      B = function(n) {
+        return 1 - A(1 - n);
+      },
+      F = function(n) {
+        return n < 0.5 ? B(2 * n) / 2 : A(2 * n - 1) / 2 + 0.5;
+      },
+      C = 4,
+      D = 0.001,
+      E = 1e-7,
+      G = 10,
+      H = 11,
+      J = 1 / (H - 1),
+      K = function(n, t) {
+        return 1 - 3 * t + 3 * n;
+      },
+      L = function(n, t) {
+        return 3 * t - 6 * n;
+      },
+      N = function(n) {
+        return 3 * n;
+      };
+    function Q(n, t, r) {
+      return (K(t, r) * n + L(t, r)) * n + N(t);
+    }
+    function R(n, t, r) {
+      return 3 * K(t, r) * n * n + 2 * L(t, r) * n + N(t);
+    }
+    function U(n, t, r, u) {
+      if (n === t && r === u) return i;
+      for (var e = new Float32Array(H), o = 0; o < H; o++) e[o] = Q(o * J, n, r);
+      function c(t) {
+        for (var u = 0, i = 1, o = H - 1; i !== o && e[i] < t; i++) u += J;
+        var c = u + ((t - e[(i -= 1)]) / (e[i + 1] - e[i])) * J,
+          f = R(c, n, r);
+        return f >= D
+          ? (function(n, t, r, u) {
+              for (var e = t, i = 0; i < C; i++) {
+                var o = R(e, r, u);
+                if (0 === o) break;
+                e -= (Q(e, r, u) - n) / o;
+              }
+              return e;
+            })(t, c, n, r)
+          : 0 === f
+          ? c
+          : (function(n, t, r, u, e) {
+              var i = 0,
+                o = t,
+                c = r,
+                f = 0;
+              do {
+                Q((i = o + (c - o) / 2), u, e) - n > 0 ? (c = i) : (o = i);
+              } while (Math.abs(n) > E && ++f < G);
+              return i;
+            })(t, u, u + J, n, r);
+      }
+      return function(n) {
+        return Q(c(n), t, u);
+      };
+    }
+    var V = function(n) {
+        return function(t) {
+          return 1 - n(1 - t);
+        };
+      },
+      W = function(n) {
+        return function(t) {
+          return t < 0.5 ? n(2 * t) / 2 : 1 - n(2 * (1 - t)) / 2;
+        };
+      },
+      X = function(n) {
+        return function(t, r, u, e, i) {
+          return u + n(r / i) * (e - u);
+        };
+      };
+    r.d(t, 'linear', function() {
+      return i;
+    }),
+      r.d(t, 'sineIn', function() {
+        return o;
+      }),
+      r.d(t, 'sineOut', function() {
+        return c;
+      }),
+      r.d(t, 'sineInOut', function() {
+        return f;
+      }),
+      r.d(t, 'quadraticIn', function() {
+        return a;
+      }),
+      r.d(t, 'quadraticOut', function() {
+        return d;
+      }),
+      r.d(t, 'quadraticInOut', function() {
+        return s;
+      }),
+      r.d(t, 'cubicIn', function() {
+        return l;
+      }),
+      r.d(t, 'cubicOut', function() {
+        return v;
+      }),
+      r.d(t, 'cubicInOut', function() {
+        return M;
+      }),
+      r.d(t, 'quarticIn', function() {
+        return h;
+      }),
+      r.d(t, 'quarticOut', function() {
+        return p;
+      }),
+      r.d(t, 'quarticInOut', function() {
+        return O;
+      }),
+      r.d(t, 'quinticIn', function() {
+        return b;
+      }),
+      r.d(t, 'quinticOut', function() {
+        return I;
+      }),
+      r.d(t, 'quinticInOut', function() {
+        return w;
+      }),
+      r.d(t, 'exponentialIn', function() {
+        return q;
+      }),
+      r.d(t, 'exponentialOut', function() {
+        return y;
+      }),
+      r.d(t, 'exponentialInOut', function() {
+        return m;
+      }),
+      r.d(t, 'circularIn', function() {
+        return x;
+      }),
+      r.d(t, 'circularOut', function() {
+        return P;
+      }),
+      r.d(t, 'circularInOut', function() {
+        return g;
+      }),
+      r.d(t, 'backwardsIn', function() {
+        return j;
+      }),
+      r.d(t, 'backwardsOut', function() {
+        return S;
+      }),
+      r.d(t, 'backwardsInOut', function() {
+        return _;
+      }),
+      r.d(t, 'elasticIn', function() {
+        return k;
+      }),
+      r.d(t, 'elasticOut', function() {
+        return T;
+      }),
+      r.d(t, 'elasticInOut', function() {
+        return z;
+      }),
+      r.d(t, 'bounceOut', function() {
+        return A;
+      }),
+      r.d(t, 'bounceIn', function() {
+        return B;
+      }),
+      r.d(t, 'bounceInOut', function() {
+        return F;
+      }),
+      r.d(t, 'makeBezier', function() {
+        return U;
+      }),
+      r.d(t, 'reverse', function() {
+        return V;
+      }),
+      r.d(t, 'mirror', function() {
+        return W;
+      }),
+      r.d(t, 'addScaling', function() {
+        return X;
+      });
+  }
+]);
